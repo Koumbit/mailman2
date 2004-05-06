fb4566bca28242e3d42bbb9b2668cda508cef5c3
Bernd Siggy Brentrup <bsb@free-it.org>
2004-05-06 09:15:36 +0000

    Create /etc/cron.d/mailman from template and give it conffile like treatment using cfdb.

--- core/debian/Debian/__init__.py
+++ core/debian/Debian/__init__.py
@@ -2,7 +2,7 @@
 # $Id$
 
 from helpers   import *
-from templates import mm_cfg_defaults, mm_cfg_fillin
+from templates import mm_cfg_defaults, mm_cfg_fillin, mm_crontab
 from cf_db     import ConfFileDatabase
 
 mm_languages="big5 ca cs da de en es et eu fi fr gb hr hu it ja ko lt nl no pl pt pt_BR ro ru sl sr sv uk".split()
