b15ed8fb20ec9ec1b7e097f31a9cc51dc3cbe46a
Bernd Siggy Brentrup <bsb@free-it.org>
2004-05-18 16:29:43 +0000

    Install python code in /usr/lib/mailman getting rid of symlinks from /var/lib/mailman.

--- debian/Debian/__init__.py
+++ debian/Debian/__init__.py
@@ -2,8 +2,12 @@
 # $Id$
 
 from helpers   import *
-from templates import mm_cfg_defaults, mm_cfg_fillin, mm_crontab
 from cf_db     import ConfFileDatabase
+
+try:
+    from templates import mm_cfg_defaults, mm_cfg_fillin, mm_crontab
+except ImportError:
+    pass
 
 mm_languages="big5 ca cs da de en es et eu fi fr gb hr hu it ja ko lt nl no pl pt pt_BR ro ru sl sr sv uk".split()
 
--- debian/Debian/templates.py
+++ debian/Debian/templates.py
@@ -29,7 +29,7 @@ mm_cfg_defaults = '''\
 From a raw distribution it should be copied to mm_cfg.py.  If you
 already have an mm_cfg.py, be careful to add in only the new settings
 you want.  The complete set of distributed defaults, with annotation,
-are in /var/lib/mailman/Mailman/Defaults.py.  In mm_cfg, override only
+are in /usr/lib/mailman/Mailman/Defaults.py.  In mm_cfg, override only
 those you want to change, after the
 
   from Defaults import *
@@ -64,58 +64,57 @@ mm_cfg_fillin = '''\
 MAILMAN_SITE_LIST = %(MAILMAN_SITE_LIST)r
 
 #-------------------------------------------------------------
-# If you change these, you have to configure your http server
-# accordingly (Alias and ScriptAlias directives in most httpds)
+# Virtual host httpd setup, if you don't use a virtual host
+# just copy the ScriptAlias and Alias directives.
+#
+# <Virtualhost %(DEFAULT_URL_HOST)s>
+#   ServerName  %(DEFAULT_URL_HOST)s
+#   ScriptAlias %(default_url_path)s /usr/lib/cgi-bin/mailman/
+#   Alias       /pipermail/ %(PUBLIC_ARCHIVE_FILE_DIR)s/
+#   Alias       %(IMAGE_LOGOS)s /usr/share/images/mailman/
+# </VirtualHost>
+#-------------------------------------------------------------
+DEFAULT_URL_HOST   = %(DEFAULT_URL_HOST)r
+DEFAULT_EMAIL_HOST = %(DEFAULT_EMAIL_HOST)r
+add_virtualhost(DEFAULT_URL_HOST, DEFAULT_EMAIL_HOST)
 DEFAULT_URL_PATTERN = %(DEFAULT_URL_PATTERN)r
-# For logos on MM web pages, add this to your httpd config:
-#   Alias %(IMAGE_LOGOS)s /usr/share/images/mailman/
 IMAGE_LOGOS         = %(IMAGE_LOGOS)r
 
 #-------------------------------------------------------------
-# Default host for web interface of newly created MLs
-DEFAULT_URL_HOST   = %(DEFAULT_URL_HOST)r
+# Depending on your MTA setup, the MTA configuration variable
+# controls how aliases are generated.
+# At the time of this writing legal values are:
+# None      - your MTA automatically recognizes new lists
+# 'Postfix' - you are using postfix virtual domains
+# 'Manual'  - default, meaning you have to manually create the
+#             aliases in /etc/aliases or similar.
+# In any case, please read the MTA specific README.* in
+# /usr/share/doc/mailman before setting this.
+MTA=%(MTA)r
+
 #-------------------------------------------------------------
-# Default domain for email addresses of newly created MLs
-DEFAULT_EMAIL_HOST = %(DEFAULT_EMAIL_HOST)r
+# The USE_ENVELOPE_SENDER variable controls the order in which
+# headers are searched when determining if a message originates
+# from a subscriber.  When True, the order is Sender:, From:,
+# unixfrom, when False it is From:, Sender:, unixfrom.  In both
+# cases the first address encountered is used.
+# Debian default is not to use the envelope address when
+# determining if a message comes from a subscriber.
+# This option applies globally to all mailing lists.
+USE_ENVELOPE_SENDER    = %(USE_ENVELOPE_SENDER)r
+
 #-------------------------------------------------------------
-# Required when setting any of its arguments.
-add_virtualhost(DEFAULT_URL_HOST, DEFAULT_EMAIL_HOST)
+# Debian default is not to send monthly password reminders
+# on newly created lists.  This can be changed per list.
+DEFAULT_SEND_REMINDERS = %(DEFAULT_SEND_REMINDERS)r
 
 #-------------------------------------------------------------
 # The default language for this server (may be changed to a
 # supported language when mailman-i18n is installed).
 DEFAULT_SERVER_LANGUAGE = %(DEFAULT_SERVER_LANGUAGE)r
 
-#-------------------------------------------------------------
-# Don't use address from envelope instead of from headers when
-# determining if a message comes from a list member.
-USE_ENVELOPE_SENDER    = %(USE_ENVELOPE_SENDER)r
-
-#-------------------------------------------------------------
-# By default don't send monthly password reminders for newly
-# created lists.
-DEFAULT_SEND_REMINDERS = %(DEFAULT_SEND_REMINDERS)r
-
-#-------------------------------------------------------------
-# Uncomment this if you configured your MTA such that it
-# automatically recognizes newly created lists.
-# (see /usr/share/doc/mailman/README.{EXIM,...})
-# MTA=None   # Misnomer, suppresses alias output on newlist
-
-#-------------------------------------------------------------
-# Uncomment if you use Postfix virtual domains, but be sure to
-# read /usr/share/doc/mailman/README.POSTFIX first.
-# MTA='Postfix'
-
 # Note - if you're looking for something that is imported from mm_cfg, but you
 # didn't find it above, it's probably in /usr/lib/mailman/Mailman/Defaults.py.
-
-#-------------------------------------------------------------
-# Local settings not managed by Debian maintainer scripts. 
-# only variables not mentioned above are preserved on package
-# upgrade. If you define variables mentioned above here,
-# the last setting here will be used above and occurrences
-# below are removed.
 '''#
 
 mm_crontab = '''\
@@ -126,25 +125,25 @@ mm_crontab = '''\
 # At 8AM every day, mail reminders to admins as to pending requests.
 # They are less likely to ignore these reminders if they're mailed
 # early in the morning, but of course, this is local time... ;)
-0 8 * * * list /var/lib/mailman/cron/checkdbs
+0 8 * * * list /usr/lib/mailman/cron/checkdbs
 #
 # At 9AM, send notifications to disabled members that are due to be
 # reminded to re-enable their accounts.
-0 9 * * * list /var/lib/mailman/cron/disabled
+0 9 * * * list /usr/lib/mailman/cron/disabled
 #
 # Noon, mail digests for lists that do periodic as well as threshhold delivery.
-0 12 * * * list /var/lib/mailman/cron/senddigests
+0 12 * * * list /usr/lib/mailman/cron/senddigests
 #
 # 5 AM on the first of each month, mail out password reminders.
-0 5 1 * * list /var/lib/mailman/cron/mailpasswds
+0 5 1 * * list /usr/lib/mailman/cron/mailpasswds
 #
 # Every 5 mins, try to gate news to mail.  You can comment this one out
 # if you don't want to allow gating, or don't have any going on right now,
 # or want to exclusively use a callback strategy instead of polling.
-# 0,5,10,15,20,25,30,35,40,45,50,55 * * * * list /var/lib/mailman/cron/gate_news
+# 0,5,10,15,20,25,30,35,40,45,50,55 * * * * list /usr/lib/mailman/cron/gate_news
 #
 # At 3:27am every night, regenerate the gzip'd archive file.  Only
 # turn this on if the internal archiver is used and
 # GZIP_ARCHIVE_TXT_FILES is false in mm_cfg.py
-27 3 * * * list /var/lib/mailman/cron/nightly_gzip
+27 3 * * * list /usr/lib/mailman/cron/nightly_gzip
 '''#
